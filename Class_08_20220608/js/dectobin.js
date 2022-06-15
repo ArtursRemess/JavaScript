edd90f6c4207ae2e2c578788e1bf3c35ce3caf8a
ArtursRemess <siarefita@gmail.com>
2022-06-15 16:51:44 +0300

    letter

--- Class_08_20220608/js/dectobin.js
+++ Class_08_20220608/js/dectobin.js
@@ -1,31 +1,34 @@
-var xo = 53,
-    y = null,
-    z = String(y),
-    binars = 0;
-    rezultats = String(binars);
+var decim = 88,
+  binars = "";
+rezultats = String(binars);
+masivs = [];
+masiva_rez = "";
 
-document.getElementById("skaitlis_").innerHTML = xo;
+document.getElementById("skaitlis_").innerHTML = decim;
+document.getElementById("parbaude_").innerHTML = decim.toString(2);
+console.log("Pārbaude  " + decim.toString(2));
 
-document.getElementById("parbaude_").innerHTML = xo.toString(2);
-console.log('pārbaude  ' + xo.toString(2));
+//*  console.log(i);
+//*  console.log(decim, (binars = decim - 2 * (decim >> 1)), (decim = decim >> 1));
 
 
-console.log(xo,binars=xo-(2*(xo>>1)),xo=xo>>1);
-rezultats=String(binars)+rezultats;
-console.log(xo,binars=xo-(2*(xo>>1)),xo=xo>>1);
-rezultats=String(binars)+rezultats;
-console.log(xo,binars=xo-(2*(xo>>1)),xo=xo>>1);
-rezultats=String(binars)+rezultats;
-console.log(xo,binars=xo-(2*(xo>>1)),xo=xo>>1);
-rezultats=String(binars)+rezultats;
-console.log(xo,binars=xo-(2*(xo>>1)),xo=xo>>1);
-rezultats=String(binars)+rezultats;
-console.log(xo,binars=xo-(2*(xo>>1)),xo=xo>>1);
-rezultats=String(binars)+rezultats;
-console.log(xo,binars=xo-(2*(xo>>1)),xo=xo>>1);
-rezultats=String(binars)+rezultats;
-console.log(xo,binars=xo-(2*(xo>>1)),xo=xo>>1);
-rezultats=String(binars)+rezultats;
-console.log(rezultats);
+for (let i = 8; i > 0; i--) {
+  binars = decim - 2 * (decim >> 1);
+  decim = decim >> 1;
+  masivs[i] = binars;
+  }
 
-document.getElementById("rezultats_").innerHTML = rezultats;
+console.log("Masīva garums = " + masivs.length);
+
+for (let i = 1; i < masivs.length; i++) {
+  console.log(i + ". masīva elements=" + masivs[i]);
+  masiva_rez = masiva_rez + masivs[i];
+}
+
+console.log("Masīva rezultāts(BIN) = " + masivs.join(''));
+
+//*console.log("Masīva rezultāts(BIN) = " + masiva_rez);
+//* rezultats = String(binars) + rezultats;
+//* console.log(rezultats);
+
+document.getElementById("rezultats_").innerHTML = masiva_rez;
---
+++ Class_09_20220613/js/script.js
@@ -0,0 +1,33 @@
+var 
+    vards = 'Ivars Godmanis',
+    summa = 10000,
+    procenti=0,
+    teksts = '';
+ 
+
+teksts='Cien. ' + vards + '<br>';
+teksts=teksts+'Mēs redzam ka jūsu kontā ir '+summa+ ' EUR liela naudas summa<br>';
+
+teksts=teksts+'Ja jūs ieguldīsiet uz 1 gadu, 1.gadā jūs nopelnīsiet ' +(Math.round(summa*0.01));
+summa=summa+(Math.round(summa*0.01));
+teksts=teksts+' EUR un  jums būs '+(Math.round(summa))+' EUR<br>';
+
+teksts=teksts+'Ja jūs ieguldīsiet uz 2 gadiem, 2.gadā jūs nopelnīsiet '+(Math.round(summa*0.02));
+summa=summa+(Math.round(summa*0.02));
+teksts=teksts+' EUR un  jums būs '+(Math.round(summa))+' EUR<br>';
+
+teksts=teksts+'Ja jūs ieguldīsiet uz 3 gadiem, 3.gadā jūs nopelnīsiet '+(Math.round(summa*0.03));
+summa=summa+(Math.round(summa*0.03));
+teksts=teksts+' EUR un  jums būs '+(Math.round(summa))+' EUR<br>';
+
+teksts=teksts+'Ja jūs ieguldīsiet uz 4 gadiem, 4.gadā jūs nopelnīsiet '+(Math.round(summa*0.04));
+summa=summa+(Math.round(summa*0.04));
+teksts=teksts+' EUR un  jums būs '+(Math.round(summa))+' EUR<br>';
+
+teksts=teksts+'Ja jūs ieguldīsiet uz 5 gadiem, 5.gadā jūs nopelnīsiet '+(Math.round(summa*0.05));
+summa=summa+(Math.round(summa*0.05));
+teksts=teksts+' EUR un  jums būs '+(Math.round(summa))+' EUR<br>';
+
+console.log(teksts);
+document.getElementById("vestule").innerHTML = teksts;
+
