491a2f6b57e8b20231f8bc61d748184a866c2a58
ArtursRemess <siarefita@gmail.com>
2022-06-20 20:19:24 +0300

    ABC

--- Class_11_20220620_if_statement/js/script.js
+++ Class_11_20220620_if_statement/js/script.js
@@ -1,42 +1,48 @@
-var vards = "Ivars Godmanis",
-    language = "LV",
-    age = 12,
-    a = 0,
-    teksts = "",
-    greeting='',
-    time=10;
+var c=71,
+   d=43,
+   e=88;
 
-if (age > 10 && language == "LV") {
-    console.log("Tev ir vairāk nekā 10 gadi un  LV");
-} else {
-    console.log("ir " + a + " Speak English");
-};
-
-if (time < 10) 
-    {
-    greeting = "Good morning";
-    } 
-else 
-   if (time < 20) 
+if (c>d && c>e)
    {
-    greeting = "Good day";
-    }  
-   else 
+    console.log("Vislielākais ir c=" +c )
+    if (d>e)
+        {
+          console.log("Nākamais ir d=" +d )
+          console.log("Mazākais ir e=" +e )
+        }
+        else
+        {
+            console.log("Nākamais ir e=" +e )
+            console.log("Mazākais ir d=" +d )
+        }
+    
+   }
+else if (d>c && d>e)
    {
-    greeting = "Good evening";
+    console.log("Vislielākais ir d=" +d )
+    if (c>e)
+        {
+          console.log("Nākamais ir c=" +c )
+          console.log("Mazākais ir e=" +e )
+        }
+        else 
+        {
+          console.log("Nākamais ir e=" +e )
+          console.log("Mazākais ir c=" +c )
+        }
+   }
+   else
+   {
+    console.log("Vislielākais ir e=" +e )
+    if (d>c)
+        {
+          console.log("Nākamais ir d=" +d )
+          console.log("Mazākais ir c=" +c )
+        }
+        else
+        { 
+          console.log("Nākamais ir c=" +c )
+          console.log("Mazākais ir d=" +d )
+        }
    }
 
-
-
-
-
-// if (a) {console.log( a+ " nav nulle")}
-// else
-// {
-//      {console.log("ir " + a+ " nulle")}
-// };
-
-//     teksts='aa' ;
-
-// console.log(teksts);
-// document.getElementById("vestule").innerHTML = teksts;
