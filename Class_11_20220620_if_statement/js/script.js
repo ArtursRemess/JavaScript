979efbb755e7ad7560e3cae34732a0b0202069f9
ArtursRemess <siarefita@gmail.com>
2022-06-20 19:23:35 +0300

    sa

---
+++ Class_11_20220620_if_statement/js/script.js
@@ -0,0 +1,42 @@
+var vards = "Ivars Godmanis",
+    language = "LV",
+    age = 12,
+    a = 0,
+    teksts = "",
+    greeting='',
+    time=10;
+
+if (age > 10 && language == "LV") {
+    console.log("Tev ir vairāk nekā 10 gadi un  LV");
+} else {
+    console.log("ir " + a + " Speak English");
+};
+
+if (time < 10) 
+    {
+    greeting = "Good morning";
+    } 
+else 
+   if (time < 20) 
+   {
+    greeting = "Good day";
+    }  
+   else 
+   {
+    greeting = "Good evening";
+   }
+
+
+
+
+
+// if (a) {console.log( a+ " nav nulle")}
+// else
+// {
+//      {console.log("ir " + a+ " nulle")}
+// };
+
+//     teksts='aa' ;
+
+// console.log(teksts);
+// document.getElementById("vestule").innerHTML = teksts;
