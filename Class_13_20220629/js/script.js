ee0dbf3304793a53dd9afee162db260dfef7d3ab
ArtursRemess <siarefita@gmail.com>
2022-06-29 20:41:40 +0300

    for_for_2

--- Class_13_20220629/js/script.js
+++ Class_13_20220629/js/script.js
@@ -1,10 +1,11 @@
-var   text = "";
+var   text = "",
+      dalit=[2,3,4,6,8];
 for (let i = 1; i < 25; i++) 
 {
   text = text+ i+ " dalās ar skaitli ";
-  for (var k=2; k<7; k++ )
-      if (i % k == 0) {
-      text = text+k+",";
+  for (var k=0; k<dalit.length; k++ )
+      if (i % dalit[k] == 0) {
+      text = text+dalit[k]+",";
      }
     
     if (i%2 == 0)
@@ -15,8 +16,6 @@ for (let i = 1; i < 25; i++)
     {
       text = text +  " nepāra skaitlis "
     }
-  
-  text=text+"<BR>";
+    text=text+"<BR>";
 }
-
 document.getElementById("automatic_count").innerHTML = text;
