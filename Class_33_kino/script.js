990db44e71c5dc8f12287e0c5a31ac6d71e2092f
ArtursRemess <siarefita@gmail.com>
2022-09-19 01:26:30 +0300

    kino

--- Class_33_kino/script.js
+++ Class_33_kino/script.js
@@ -1,6 +1,31 @@
 ﻿var seats = []; //sēdvietu masīvs
-const seat_rows = 5; //sēdvietu rindu skaits
-const seat_seats = 10; //sēdvietas rindā
+var seats_occupied = []; //aizņemtās sēdvietas
+const seat_rows = 4; //sēdvietu rindu skaits
+const seat_seats = 8; //sēdvietas rindā
+const seats_total = (seat_seats * seat_rows); // kopējais sēdvietu skaits
+const seats_occupied_total = Math.floor(Math.random() * (seats_total + 1)) ; //aizņemto sēdvietu skaits
+//const seats_occupied_total = 10 ;
+console.log("total occupied " + seats_occupied_total);
+console.log("total seats " + seats_total);
+
+
+function create_random_seats(seats_total, seats_occupied_total){ //izveido masīvu ar aizņemtajiem krēsliem
+  var temp_string=[];
+  for (let i = 0; i < seats_total; i++)  // izveido pagaidu rindu ar masīva indexiem
+  { 
+    temp_string.push(i) ;
+  }
+  let id_todel=0;
+  let xxx = [];
+  for (let i = 0; i < seats_occupied_total; i++){  // izvēlas gadījuma ierakstus un pārnes jaunajā masīvā
+      id_todel=(Math.floor(Math.random() * (temp_string.length)));
+  //    console.log("id_todel= " + id_todel + " temp_string.at(id_todel) " + temp_string.at(id_todel) + "  tmp_lenth= " + temp_string.length);
+      xxx.push(Number(temp_string.splice(id_todel,1)));  
+  } 
+   console.log("seats_not_occupied");
+   console.log(temp_string);
+   return xxx;
+}
 
 for (let i = 1; i < seat_rows+1; i++)  // izveido masīvu ar sēdvietu numuru sarakstu
   {
@@ -9,13 +34,21 @@ for (let i = 1; i < seat_rows+1; i++)  // izveido masīvu ar sēdvietu numuru sa
       seats.push([i,j]);
     }
   }
-  var div_kino = document.getElementById('kino'); //sēdvietu zāle
+
+ seats_occupied = create_random_seats(seats_total, seats_occupied_total);
+ console.log("seats_occupied");
+ console.log(seats_occupied);
+
+  const div_kino = document.getElementById('kino'); //sēdvietu zāle
   div_kino.insertAdjacentHTML("beforeend", "<div class='row' id='row'></div>"); // izveido 1.rindu
   var div_row = div_kino.lastElementChild; // atlasa pēdējo child no "kino"
-
   i = 1;
   seats.forEach((seat,current_id) => {
-    div_row.insertAdjacentHTML("beforeend", "<div class='seat' title='"+ seat.at(0) +".row, "+ seat.at(1) +".seat'></div>");
+    if (seats_occupied.includes(current_id)){  //ja aiņemts tad
+      div_row.insertAdjacentHTML("beforeend", "<div class='seat occupied' title='seat id="+current_id+"'></div>");
+    }else{
+     div_row.insertAdjacentHTML("beforeend", "<div class='seat' title='"+ seat.at(0) +".row, "+ seat.at(1) +". seat id="+current_id+"'></div>");
+    };
     if (i < seat_seats){  //skaita krēslus rindā, ja sasniegts skaits veido jaunu rindu
       i++;
     }
@@ -26,7 +59,7 @@ for (let i = 1; i < seat_rows+1; i++)  // izveido masīvu ar sēdvietu numuru sa
     }  
   });
 
- console.table(seats);
+ // console.table(seats);
 
 /* const container = document.querySelector('.container'); // dati no container bloka (krēsli)
 
