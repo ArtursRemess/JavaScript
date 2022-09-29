352aabbf50e5cc2170ea4091c05bcce85ecd79c9
ArtursRemess <siarefita@gmail.com>
2022-09-29 20:56:04 +0300

    projekts

--- Projekts/admin/js/back.js
+++ Projekts/admin/js/back.js
@@ -18,7 +18,7 @@ const div_movies = document.getElementById('movies');
 var i = 0;
 movies.forEach((movie,movie_index) => {
     i++;
-   div_movies.insertAdjacentHTML("beforeend", "<span class='poster' ><a href='back_movie.html?id=" + movie_index + "'><img height='330' src= './poster/"+ movies[movie_index].at(3)+ "' alt='Poster'><h5>" + movies[movie_index].at(2) + "</h5></a></span>");
+   div_movies.insertAdjacentHTML("beforeend", "<span class='poster' ><a href='back_movie.html?id=" + movie_index + "'><img height='330' src= '../poster/"+ movies[movie_index].at(3)+ "' alt='Poster'><h5>" + movies[movie_index].at(2) + "</h5></a></span>");
     if (i == 3){ // pēc 3 filmām jauna rinda
         div_movies.insertAdjacentHTML("beforeend", "<br>");
         i = 0;
